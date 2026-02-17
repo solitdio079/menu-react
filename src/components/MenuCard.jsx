5c3c827f3c988ceb22484f0f55e79c4f1e411a47
Djoko Keita <djokokeita@Djokos-MacBook-Air-2.local>
2026-02-17 12:57:31 +0300

    Add bigger cards

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,6 +1,6 @@
 export default function MenuCard({imgSrc, name, description,children}) {
     return (<>
-        <div className="card">
+        <div className="card w-full bg-base-100 shadow-xl">
             <figure><img src={imgSrc} alt={name} /></figure>
             <div className="card-body">
                 <h5 className="card-title mb-2.5"> {name} </h5>
--- src/components/Menus.jsx
+++ src/components/Menus.jsx
@@ -4,7 +4,7 @@ import MenuCard from "./MenuCard";
 import { Link } from "react-router-dom";
 export default function Menus() {
     return (<>
-        <div className="grid lg:grid-cols-4 gap-4 p-10 my-6 items-center justify-center">
+        <div className="grid lg:grid-cols-4 gap-4 my-6 items-center justify-center">
             {menu.mainDishes.map((item, index) => (<Fragment key={index}><MenuCard imgSrc={item.image} name={item.name} description={item.description}><Link to={`/details/${index}/${item.excluded}`} className="btn btn-primary"> <span className="icon-[tabler--eye]"></span> </Link></MenuCard></Fragment>))}
         </div>
     </>)
