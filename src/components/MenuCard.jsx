9faf0fe89e9a4ce05d82adde1dc7f1d069ea0b6a
Djoko Keita <djokokeita@Djokos-MacBook-Air-2.local>
2026-02-17 21:50:11 +0300

    Add bigger cards

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";
 export default function MenuCard({imgSrc, name, description,children}) {
     return (<>
         <div className="card w-full bg-base-100 shadow-xl">
-            <figure><img src={imgSrc} alt={name} /></figure>
+          <figure><img src={imgSrc} alt={name} /></figure>
             <div className="card-body">
                 <h5 className="card-title mb-2.5"> {name} </h5>
                 <p className="mb-4"> {description} </p>
